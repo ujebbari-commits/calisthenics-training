a257189fc51a3722a5f40840362e37f0a876d22c
ujebbari-commits <ujebbari@gmail.com>
2026-09-12 18:37:38 +0900

    Cache PNG install icons

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,17 @@
-const CACHE='calisthenics-v7';
-const ASSETS=['./','./index.html','./styles.css?v=20260912-3','./app.js?v=20260912-3','./app-v3.js?v=20260912-3','./app-v4.js?v=20260912-3','./manifest.webmanifest?v=20260912-3','./icon.svg'];
+const CACHE='calisthenics-v8';
+const ASSETS=[
+  './',
+  './index.html',
+  './styles.css?v=20260912-4',
+  './app.js?v=20260912-4',
+  './app-v3.js?v=20260912-4',
+  './app-v4.js?v=20260912-4',
+  './manifest.webmanifest?v=20260912-4',
+  './icon.svg',
+  './icon-180.png',
+  './icon-192.png',
+  './icon-512.png'
+];
 
 self.addEventListener('install',e=>{
   self.skipWaiting();
