1022fc659d674603266520c82f45820c21916913
ujebbari-commits <ujebbari@gmail.com>
2026-09-12 18:28:15 +0900

    Cache Yomitan and Google Images workout UX

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='calisthenics-v5';
-const ASSETS=['./','./index.html','./styles.css?v=20260912-1','./app.js?v=20260912-1','./app-v3.js?v=20260912-1','./manifest.webmanifest?v=20260912-1','./icon.svg'];
+const CACHE='calisthenics-v6';
+const ASSETS=['./','./index.html','./styles.css?v=20260912-2','./app.js?v=20260912-2','./app-v3.js?v=20260912-2','./app-v4.js?v=20260912-2','./manifest.webmanifest?v=20260912-2','./icon.svg'];
 
 self.addEventListener('install',e=>{
   self.skipWaiting();
@@ -23,5 +23,11 @@ self.addEventListener('fetch',e=>{
     }).catch(()=>caches.match('./index.html')));
     return;
   }
-  e.respondWith(caches.match(req).then(r=>r||fetch(req)));
+  e.respondWith(fetch(req).then(r=>{
+    if(r&&r.ok&&req.method==='GET'){
+      const copy=r.clone();
+      caches.open(CACHE).then(c=>c.put(req,copy));
+    }
+    return r;
+  }).catch(()=>caches.match(req)));
 });
